97fbfdbaffad1f607a5cd0ba93b000f4a625d80a
v.kapustin <vkapustin@bivgroup.ru>
2025-04-28 16:40:58 +0300

    add: questions request

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { HttpClientService } from "../services/http-client.service";
+import { MainService } from "../services/main.service";
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,16 @@ import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/rou
 })
 export class AppComponent {
 
-  enabled:boolean = false;
+  enabled: boolean = false;
 
-  constructor(private router: Router){}
+  constructor(
+    private router: Router,
+    private mainService: MainService,
+  ) {
+    this.mainService.getQuestions(undefined).subscribe();
+  }
 
-  goStart(){
+  goStart() {
     this.router.navigate(["answerer"]);
     this.enabled = true;
   }
